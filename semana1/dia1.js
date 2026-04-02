5f280a80f93b10b57ea1b667f0d582e396ceccae
Luis-Miranda <estevezluis758@gmail.com>
2026-04-01 22:31:48 -0600

    Día 1 y 2: variables, strings y template literals

---
+++ semana1/dia1.js
@@ -0,0 +1,10 @@
+const nombre = "Luis";
+const apellido = "Miranda";
+const edad = 24;
+const estudiando = true;
+const ciudad = "CDMX";
+
+
+console.log(nombre)
+console.log(nombre + " "+apellido)
+console.log(nombre + " de : "+ ciudad)
---
+++ semana1/dia2.js
@@ -0,0 +1,13 @@
+const nombre = "Luis";
+const apellido = "Miranda";
+const edad = 24;
+const estudiando = true;
+const ciudad = "CDMX";
+
+
+const msg = `Hola! mi nombre es ${nombre}, soy de ${ciudad}`;
+const msg1 = `soy de ${ciudad} y tengo ${edad}`
+console.log(msg)
+console.log(msg1)
+console.log(nombre.toUpperCase())
+console.log(apellido.length)
